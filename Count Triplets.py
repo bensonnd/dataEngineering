bd4ca969755c2d17717daa3e2097865721e965b3
NEIL BENSON <Neil.Benson@aa.com>
2020-02-22 12:13:06 -0600

    HackerRank practice  - Counting Triplets - update to logic

--- Count Triplets.py
+++ Count Triplets.py
@@ -7,46 +7,31 @@ import os
 import random
 import re
 import sys
+from collections import Counter
 
-def groupingList(list):
-    """Counts the number of occurrences of each number, in order"""
-    groupbylist = []
-    for key, group in groupby(list, lambda x: x):
-        var_count = 0
-        for num in group:
-            var_count += 1
-        groupbylist.append([var_count, key])
-    return groupbylist
+def counttripletcounters(arr, r):
+    # doubletcounter - counting potential doublet end numbers - where num in arr is left side of doublet,
+    # if num 1 in arr, potential future doublet would be (1,2), and with 2, (2,4), and so on...
+    doubletcounter = Counter()
+    #tripletcounter - counting potential triplet end numbers - if num in arr is end of doublet, multiply by r, and count as a potential
+    #triplet - if num in arr is 2, and is end of doublet, add 2 * r as potential right side of triplet
+    tripletcounter = Counter()
+    count = 0
 
-def geometric_progression(list,r):
-    """Checks the geometric progression of each triplet within the list, in order"""
-    if (list[1][1] == list[0][1] * r ** 1) and (list[2][1] == list[0][1] * r ** 2): return True
-    else: return False
+    for num in arr:
+        if num in tripletcounter:
+            #increment counter where num matches end of tripletcounter and is also end of doubletcounter
+            count += tripletcounter[num]
 
-# Complete the countTriplets function below.
-def countTriplets(arr, r):
-    grouplist = groupingList(arr)
-    countlist = []  #doesn't check against previous
-    #create triplets from grouped list ex. [[1,1],[2,2],[3,2],[1,4]]
-    # gives [[1,1],[2,2],[3,2]] and [[2,2],[3,2],[1,4]]
-    for i in range(len(grouplist)-2):
-        triplet = [grouplist[i],grouplist[i+1],grouplist[i+2]]
-        countlist.append(triplet)
-    tripletcount = []
-    #check each succession of geometric progression
-    newcountlist = []
-    for i in countlist:
-        templistcount = []
-        #drop any triplet that is not a geometric progression
-        if geometric_progression(i,r):
-            for jind, j in enumerate(i):
-                templistcount. append(i[jind][0])
-            newcountlist.append(templistcount)
-    for i in newcountlist:
-        #multiply the grouped, filtered geometric progressions by key count from groupingList
-        prod = reduce(lambda x, y: x*y, [j for j in i])
-        tripletcount.append(prod)
-    return sum(tripletcount)
+        if num in doubletcounter:
+            # increment tripletcounter where num matches end of doublet, for future potential numbers to match end of triplet
+            testcount = doubletcounter[num]
+            tripletcounter[num * r] += testcount
+
+        #num to doubletcounter for future potential numbers to match end of doublet
+        doubletcounter[num * r] += 1
+
+    return count
 
 #arr[0]*r^(5-1)
 
@@ -55,5 +40,5 @@ n = int(nr[0])
 r = int(nr[1])
 arr = list(map(int, input().rstrip().split()))
 print(arr)
-ans = countTriplets(arr, r)
+ans = counttripletcounters(arr, r)
 print(ans)
