849a4076cf8d93638f9fb40785b01333c8354759
NEIL BENSON <Neil.Benson@aa.com>
2020-02-22 18:37:50 -0600

    HackerRank practice  - Way faster way to rotate an array

--- Arrays/Array Left Rotation.py
+++ Arrays/Array Left Rotation.py
@@ -1,8 +1,27 @@
 # Complete the rotLeft function below.
 def rotLeft(a, d):
-    for rotations in range(d):
-        list_end = a[0]
-        a.pop(0)
-        a.append(list_end)
+    remainder = d % len(a)
+    divisions = d // len(a)
+    reduced_rotations = remainder + divisions
+    list_end = a[:reduced_rotations - len(a)]
+    del a[:reduced_rotations - len(a)]
+    a = a + list_end
     return a
 
+def getMaxElementIndexes(a, rotate):
+    maxindexlist = []
+    for i in rotate:
+        templist = a.copy()
+        if i == 0:
+            maxindexlist.append(templist.index(max(templist)))
+        else:
+            rotate_list = rotLeft(templist,i)
+            maxindexlist.append(templist.index(max(rotate_list)))
+    return maxindexlist
+
+
+
+a = [1,2,3,4,5]
+rotate = [1,0,37]
+result = getMaxElementIndexes(a, rotate)
+print(result)
