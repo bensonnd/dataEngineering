91472c04c51876ad688bd7d40c64d0518a39a7d3
Neil Benson <bensonnd@gmail.com>
2020-03-15 17:16:40 -0500

    Optimizing time on heapsort. Optimized building heap.

--- Data Validation/priceCheck.py
+++ Data Validation/priceCheck.py
@@ -1,8 +1,8 @@
 def priceCheck(products, productPrices, productSold, soldPrice):
-    productsdict = dict(zip(products, productPrices))
-    productsolddict = dict(zip(productSold, soldPrice))
-    shared_items = {k: productsdict[k] for k in productsdict if k in productsolddict and productsdict[k] != productsolddict[k]}
-    return len(shared_items)
+    productsdict = dict(zip(products, productPrices))                # zip the products lookup list together and create a dictionary
+    productsolddict = dict(zip(productSold, soldPrice))              # zip the product purchases list together and create a dictionary
+    shared_items = {k: productsdict[k] for k in productsdict if k in productsolddict and productsdict[k] != productsolddict[k]} # compare the dictionaries
+    return len(shared_items)                                        # return the delta between the two dictionaries
 
 
 products = ['rice', 'sugar', 'wheat', 'cheese', 'water', 'rice']
